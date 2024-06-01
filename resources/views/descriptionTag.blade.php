f19eff9951a15c13a86c3dd66f2556181362d168
bruno <bruno.resende@gmx.com>
2024-06-01 15:54:43 -0300

    fix: add more tags to the head

--- resources/views/descriptionTag.blade.php
+++ resources/views/descriptionTag.blade.php
@@ -1,2 +1,6 @@
-<meta name="description"
-      content="Portal oficial da Academia de Bombeiros de Goiás, utilizado para controle acadêmico, gestão de presença, aplicação de provas e demais questões acadêmicas.">
+<meta name="description" content="Portal oficial da Academia de Bombeiros de Goiás, utilizado para controle acadêmico, gestão de presença, aplicação de provas e demais questões acadêmicas.">
+<meta property="og:title" content="Fenix - Sistema de Gestão Militar">
+<meta property="og:type" content="website">
+<meta property="og:site" content="Fenix">
+<meta property="og:url" content="https://fenix.cbmgo.org">
+<meta property="og:description" content="Portal oficial da Academia de Bombeiros de Goiás, utilizado para controle acadêmico, gestão de presença, aplicação de provas e demais questões acadêmicas.">
