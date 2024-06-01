193dae45d58f3fcf4b64fd987e0999c59939c796
bruno <bruno.resende@gmx.com>
2024-06-01 14:49:43 -0300

    fix: change naming

--- app/Filament/Resources/SwitchShiftResource.php
+++ app/Filament/Resources/SwitchShiftResource.php
@@ -183,7 +183,7 @@ class SwitchShiftResource extends Resource
                     ->columns(2)
                     ->schema([
                         Checkbox::make('accepted')
-                            ->label('Aceito a solicitação de troca de serviço.')
+                            ->label('Aceito a presente solicitação de troca de serviço.')
                             ->required(),
                     ]),
 
