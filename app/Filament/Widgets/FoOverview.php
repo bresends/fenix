343b95cd1d9d8db84b148ba962df1e1368470c9b
bruno <bruno.resende@gmx.com>
2024-06-08 19:21:33 -0300

    feat: basic graphs

--- app/Filament/Widgets/FoOverview.php
+++ app/Filament/Widgets/FoOverview.php
@@ -2,8 +2,12 @@
 
 namespace App\Filament\Widgets;
 
+use App\Enums\StatusEnum;
+use App\Enums\StatusFoEnum;
 use App\Models\Fo;
+use App\Models\Leave;
 use App\Models\Military;
+use App\Models\SwitchShift;
 use Filament\Widgets\StatsOverviewWidget as BaseWidget;
 use Filament\Widgets\StatsOverviewWidget\Stat;
 
@@ -12,8 +16,18 @@ class FoOverview extends BaseWidget
     protected function getStats(): array
     {
         return [
-            Stat::make('Total de Militares', Military::all()->count()),
-            Stat::make('Total de FOs', Fo::all()->count()),
+            Stat::make('Militares', Military::all()->count())
+                ->descriptionIcon('heroicon-o-user-group')
+                ->description('total de militares cadastrados'),
+            Stat::make('FOs', Fo::where('status', StatusFoEnum::EM_ANDAMENTO->value)->count())
+                ->descriptionIcon('heroicon-o-check-circle')
+                ->description('aguardando parecer'),
+            Stat::make('Dispensas', Leave::where('status', StatusEnum::EM_ANDAMENTO->value)->count())
+                ->descriptionIcon('heroicon-o-check-circle')
+                ->description('aguardando parecer'),
+            Stat::make('Trocas de Serviço', SwitchShift::where('status', StatusEnum::EM_ANDAMENTO->value)->count())
+                ->descriptionIcon('heroicon-o-check-circle')
+                ->description('aguardando parecer'),
         ];
     }
 }
