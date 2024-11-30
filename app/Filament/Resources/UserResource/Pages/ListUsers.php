e8d4ebb7169300c6ba4f741698166db40b03200e
bruno <bruno.resende@gmx.com>
2024-11-30 18:53:49 -0300

    fix: add new user button back

--- app/Filament/Resources/UserResource/Pages/ListUsers.php
+++ app/Filament/Resources/UserResource/Pages/ListUsers.php
@@ -3,29 +3,29 @@
 namespace App\Filament\Resources\UserResource\Pages;
 
 use App\Filament\Resources\UserResource;
+use Filament\Actions;
 use Filament\Actions\Action;
-use Filament\Actions\ActionGroup;
 use Filament\Resources\Pages\ListRecords;
 use Filament\Support\Colors\Color;
-use Filament\Tables\Actions\Action as TableAction;
 
-class ListUsers extends ListRecords
-{
+class ListUsers extends ListRecords {
     protected static string $resource = UserResource::class;
 
-    protected function getHeaderActions(): array
-    {
+    protected function getHeaderActions(): array {
         return [
+            Actions\CreateAction::make(),
             Action::make('download-school')
-                ->icon('heroicon-o-document-text')
-                ->label('Gerar Relatório')
-                ->url(route('absent-excel'))
-                ->label('Gerar relatório de ausências')
-                ->button()
-                ->color(Color::Zinc)
-                ->icon('heroicon-o-arrow-down-tray')
-                ->hidden(!auth()->user()->hasAnyRole(['super_admin', 'admin']))
-                ->openUrlInNewTab()
+                  ->icon('heroicon-o-document-text')
+                  ->label('Gerar Relatório')
+                  ->url(route('absent-excel'))
+                  ->label('Gerar relatório de ausências')
+                  ->button()
+                  ->color(Color::Zinc)
+                  ->icon('heroicon-o-arrow-down-tray')
+                  ->hidden(!auth()
+                      ->user()
+                      ->hasAnyRole(['super_admin', 'admin']))
+                  ->openUrlInNewTab()
         ];
     }
 }
